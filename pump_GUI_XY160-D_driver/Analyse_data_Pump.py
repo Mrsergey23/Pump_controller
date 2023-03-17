d9de8124ca85533f15b0f146616bdca2aecda094
Sergey Zharkikh <zharkih-sergey@mail.ru>
2023-03-17 12:24:14 +0300

    добавил немного офрмления

--- pump_GUI_XY160-D_driver/Analyse_data_Pump.py
+++ pump_GUI_XY160-D_driver/Analyse_data_Pump.py
@@ -2,12 +2,12 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 from statistics import mean
-
+angle_open = 80
 data_current = []
 data_flow = []
 dir_name = "C:\\Users\\General\\Documents\\m_UNI\\11semestr\\ResearchWork_NIRS\\course_work\\Pump_controller\\data_from_experiments\\"
 for i in range(40, 60, 5):
-    file_name = dir_name +"data_from_sensors"+ str(i) + "angle_valve_80.csv"
+    file_name = dir_name +"data_from_sensors"+ str(i) + "angle_valve_"+ str(angle_open) + ".csv"
     with open (file_name) as r_file:
         # Создаем объект reader, указываем символ-разделитель ","
         file_reader = csv.reader(r_file, delimiter = ",")
@@ -29,7 +29,7 @@ for i in range(40, 60, 5):
                 index_finish = k
         data_current.append(data_current_ls[index_start:index_finish])
         data_flow.append(data_flow_ls[index_start:index_finish])  
-fig, ax = plt.subplots(1, 2)
+fig, ax = plt.subplots(1, 2, figsize=(8,5), gridspec_kw={'width_ratios': [4, 1]})
 legend = []
 for j in range(len(data_current)):     
     x = np.arange(0, (len(data_current[j])), 1)
@@ -37,11 +37,15 @@ for j in range(len(data_current)):
     ax[0].set_ylabel('I, мA')
     legend.append(str(40+5*j)+'Hz, '+ str("{:.2f}".format(mean(data_flow[j])))+ ' L/hour')
     ax[0].plot(x, data_current[j], linewidth=1.5)
-
+    ax[0].annotate(str(max(data_current[j])),color='grey', xy=(data_current[j].index(max(data_current[j])), max(data_current[j])), 
+                   xytext=(data_current[j].index(max(data_current[j]))+10, max(data_current[j])+10),
+             arrowprops=dict(arrowstyle="->"),
+             )
     x2 = np.arange(0, 4, 1)
     ax[1].bar(j, mean(data_flow[j]))
 ax[0].grid()
 fig.legend(legend)
+fig.suptitle("Данные при открытии крана на " + str(angle_open)+"°") 
 plt.show()
 
 
